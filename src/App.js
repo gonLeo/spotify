f32f989ea3e562feea40cd0845f3a26e676b61c3
Leonardo Gonçalves de Oliveira <leonardo.g.o.lrv@gmail.com>
2020-04-22 20:53:53 -0400

    Navegação de Playlists

--- src/App.js
+++ src/App.js
@@ -1,26 +1,31 @@
 import React from "react";
 import GlobalStyle from "./styles/global";
+import { BrowserRouter } from 'react-router-dom'
 
 import Sidebar from "./components/Sidebar";
 import Player from "./components/Player";
 import Header from "./components/Header";
 
 import { Wrapper, Container, Content } from "./styles/components";
+import Routes from './routes'
 
 function App() {
   return (
+    <BrowserRouter>
     <React.Fragment>
       <Wrapper>
         <Container>
           <Sidebar />
           <Content>
             <Header />
+            <Routes/>
           </Content>
         </Container>
         <Player />
       </Wrapper>
       <GlobalStyle />
     </React.Fragment>
+    </BrowserRouter>
   );
 }
 
---
+++ src/pages/browse/index.js
@@ -0,0 +1,33 @@
+import React from "react";
+
+import {Container, Title, List, Playlist} from './styles'
+
+const Browse = () => (
+  <Container>
+    <Title> Navegar</Title>
+    <List>
+        <Playlist href="">
+            <img src="https://img.apmcdn.org/80ef67f5910de61be7dbb1a1adfdb4253e08dad4/square/0b317c-20130929-arctic-monkeys-am-album-cover-art.jpg" alt="Playlist"/>
+            <strong>Arctic monkeys</strong>
+            <p>Só as melhores do AM</p>
+        </Playlist>
+        <Playlist href="">
+            <img src="https://img.apmcdn.org/80ef67f5910de61be7dbb1a1adfdb4253e08dad4/square/0b317c-20130929-arctic-monkeys-am-album-cover-art.jpg" alt="Playlist"/>
+            <strong>Arctic monkeys</strong>
+            <p>Só as melhores do AM</p>
+        </Playlist>
+        <Playlist href="">
+            <img src="https://img.apmcdn.org/80ef67f5910de61be7dbb1a1adfdb4253e08dad4/square/0b317c-20130929-arctic-monkeys-am-album-cover-art.jpg" alt="Playlist"/>
+            <strong>Arctic monkeys</strong>
+            <p>Só as melhores do AM</p>
+        </Playlist>
+        <Playlist href="">
+            <img src="https://img.apmcdn.org/80ef67f5910de61be7dbb1a1adfdb4253e08dad4/square/0b317c-20130929-arctic-monkeys-am-album-cover-art.jpg" alt="Playlist"/>
+            <strong>Arctic monkeys</strong>
+            <p>Só as melhores do AM</p>
+        </Playlist>
+    </List>
+  </Container>
+);
+
+export default Browse;
